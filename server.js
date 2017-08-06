edc73b60069e24cadb7ed5ef60f28c3dfcd39d5f
shivuram <30754295+shivuram@users.noreply.github.com>
2017-08-06 12:40:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article One requested and will be seved here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-Two', function (req, res) {
