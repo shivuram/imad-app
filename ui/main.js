44f5f95cfa331b12941e190f274ceccb9da36a74
shivuram <30754295+shivuram@users.noreply.github.com>
2017-08-13 21:49:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+};
+//Make a Request to counter endpoint
+var request = new XMLHttpRequest();
+request.onreadystatechange = function()
+{
+    if(request.readystate===XMLHttpRequest.DONE){
+        if(request.state === 200){
+            counter = counter+1;
+            var span = document.getElementById('count');
+            span.innerhtml = counter.tostring();
+        }
+    }
+};
