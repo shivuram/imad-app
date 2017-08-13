f57c4217d4e35dc1efb831ee74851de0c1cc738b
shivuram <30754295+shivuram@users.noreply.github.com>
2017-08-13 22:00:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,5 +9,5 @@ button.onclick = function(){
 //Render the variable in coreect span
 counter = counter +1;
 var span = document.getElementById('count');
-span.innerHtml = counter.toString();
+span.innerHTML = counter.toString();
 };
