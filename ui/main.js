fe583eb03e576134248efc922d3702c629ca6107
shivuram <30754295+shivuram@users.noreply.github.com>
2017-08-13 22:53:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,16 @@ counter = counter +1;
 var span = document.getElementById('count');
 span.innerHTML = counter.toString();
 };
+//submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
